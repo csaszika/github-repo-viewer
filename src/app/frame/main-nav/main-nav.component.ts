1d465e60ba7f073e472729769bc71cbda4f4b096
Balint Csaszar <balint.csaszar@otpbank.hu>
2019-05-20 01:33:42 +0200

    refactor: folder structure

--- src/app/frame/main-nav/main-nav.component.ts
+++ src/app/frame/main-nav/main-nav.component.ts
@@ -1,13 +1,12 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainNavComponent {
 
--- src/app/frame/shell/shell.component.ts
+++ src/app/frame/shell/shell.component.ts
@@ -4,6 +4,5 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShellComponent {}
