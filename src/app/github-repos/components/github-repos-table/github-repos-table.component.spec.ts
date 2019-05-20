b6dee4f061368eda609f829f2d54070dea685a1b
Balint Csaszar <balint.csaszar@otpbank.hu>
2019-05-20 11:17:07 +0200

    test: github repos table component

--- src/app/github-repos/components/github-repos-table/github-repos-table.component.spec.ts
+++ src/app/github-repos/components/github-repos-table/github-repos-table.component.spec.ts
@@ -2,18 +2,9 @@ import {componentTestingSetup} from 'angular-unit-component-driver';
 import {GithubReposTableComponent} from './github-repos-table.component';
 import {GithubReposTableComponentDriver} from './github-repos-table.component.driver';
 import {MockComponent} from 'ng-mocks';
-import {
-  MatCell,
-  MatHeaderCell,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatPaginator,
-  MatRow,
-  MatRowDef,
-  MatTable,
-  MatTableModule
-} from '@angular/material';
+import {MatPaginator, MatTableModule} from '@angular/material';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {SimpleChange} from '@angular/core';
 
 const componentSetup = (): GithubReposTableComponentDriver => {
   return componentTestingSetup({
@@ -35,16 +26,43 @@ describe('GithubReposTableComponent', () => {
   });
 
   describe('Initializing', () => {
-    Given(() => {
-
-    });
-
     When(() => {
+      expect(driver.componentInstance.dataSource).toBeUndefined();
       driver.detectChanges();
     });
 
-    Then(() => {
+    Then('should be created', () => {
       expect(driver.componentInstance).toBeTruthy();
     });
+  });
+
+  describe('Input changes', () => {
+    const firstValue = [{ test: 'mock' } as any];
+    const secondValue = [{ test: 'mock2' } as any];
+
+    When(() => {
+      expect(driver.componentInstance.dataSource).toBeUndefined();
+      driver.detectChanges();
+      const previousValue = null;
+      const firstChange = true;
+      driver.componentInstance.ngOnChanges({
+        githubRepos: new SimpleChange(previousValue, firstValue, firstChange)
+      });
+    });
+
+    Then('should create the datasource', () => {
+      expect(driver.componentInstance.dataSource).toBeTruthy();
+    });
+
+    Then('should update the datasource on input change', () => {
+      const firstChange = false;
+      const previousDatasource = driver.componentInstance.dataSource;
+      driver.componentInstance.ngOnChanges({
+        githubRepos: new SimpleChange(firstValue, secondValue, firstChange)
+      });
+      expect(driver.componentInstance.dataSource).toBeTruthy();
+      expect(previousDatasource).toEqual(driver.componentInstance.dataSource);
+    });
+
   });
 });
